548576e934a16f5a56f36646440bebca9ad24b97
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-11-03 18:06:19 -0400

    trying with just html/css/js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,20 @@
 var gulp = require("gulp");
-// JS Plugins
-var browserify = require("browserify");
-var babelify = require("babelify");
-var source = require("vinyl-source-stream");
+
+// Shared Plugins
 var rename = require("gulp-rename");
-var buffer = require("vinyl-buffer");
-var sourcemaps = require("gulp-sourcemaps");
+
+// JS Plugins
 var uglify = require("gulp-uglify");
 
-
-var entryJS = 'src/index.jsx'
-var appJS = 'src/app.jsx'
-// var components = 'src/components/*.jsx'
-var jsFiles = [entryJS, appJS]
+// CSS Plugins
 
 // Just a message to confirm gulp is working
-// gulp.task('message', function(){
-//     return console.log('Gulp is running...')
-// });
+gulp.task('message', function(){
+    return new Promise(function(resolve, reject) {
+        console.log("Gulp has loaded. . . ");
+    resolve();
+    })
+});
 
 // Copy the HTML to dist file
 gulp.task('copyHTML', function(){
@@ -25,28 +22,12 @@ gulp.task('copyHTML', function(){
     .pipe(gulp.dest('dist'));
 });
 
-
-gulp.task('js', function(){
-    jsFiles.map(function( entry ){
-        return browserify({
-            entries: [entry]
-        })
-        .transform(babelify, {presets: ['env']})
-        .bundle()
-        .pipe(source(entry))
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMAps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist'))
-    })
+gulp.task('css', function(){
+    // Change CSS into Minified CSS
 });
 
-// gulp.task("default", function () {
-//   return browserify("src/index.jsx")
-//     .transform(babelify)
-//     .bundle()
-//     .pipe(source("index.jsx"))
-//     .pipe(gulp.dest("dist/"));
-// });
+gulp.task('js', function(){
+    // Change JS to Minified JS
+});
+
+// gulp.task('default', [message, copyHtml, css, js]);
