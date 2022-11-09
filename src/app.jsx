02da9bef26afa8d579aa3ffc2b0175c07234a4ae
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-11-09 17:53:31 -0500

    begin work on mapping data

--- src/app.jsx
+++ src/app.jsx
@@ -16,6 +16,23 @@ class Blog extends Component {
   }
 
   render() {
+    var postArray = Array.from(document.querySelectorAll('.content-block--pageItem__inside'));
+    // function createPosts() {
+    //   postArray.map(post => ({
+    //     link: post.querySelector('.ctaLink').getAttribute('href'),
+    //     title: post.querySelector('.ctaLink').getAttribute('title'),
+    //     tags: post.querySelector('.content-block--pageItem__metadata').firstElementChild.innerHTML,
+    //     date: post.querySelector('.content-block--pageItem__metadata').lastElementChild.innerHTML,
+    //     icon: post.querySelector('.ctaLink').getAttribute('title').split('/')[3]
+    //   }))
+    // }
+    console.log(postArray)
+    // const title = postArray[0].querySelector('.ctaLink').getAttribute('HREF');
+    // console.log(title.split('/')[3]);
+    // console.log(postArray[0].querySelector('.content-block--pageItem__metadata').lastElementChild.innerHTML);
+    // var postArray = document.querySelectorAll('.content-block--pageItem__inside').map(post => ({
+    //   title: post.title, link: post.lnink, tags: post.tags
+    // }));
     return (
       <div className="arc-blog-page">
         <BlogJumbo
@@ -34,15 +51,16 @@ class Blog extends Component {
                 <div className="col-lg-6"><div className="text-right">Sort feature here</div></div>
               </div>
             </div>
-            {/* {data.map(post=> ( */}
+            {/* Figure out how to get this in react without dom manipulation */}
+            {postArray.map(post=> (
               <BlogPost 
-              title={this.data.title}
-              link={this.data.link}
-              tags={this.data.tags}
-              date={this.data.date}
-              icon={this.data.icon}
+              title={post.querySelector('.ctaLink').getAttribute('title')}
+              link={post.querySelector('.ctaLink').getAttribute('href')}
+              tags={post.querySelector('.content-block--pageItem__metadata').firstElementChild.innerHTML.slice(',')}
+              date={post.querySelector('.content-block--pageItem__metadata').lastElementChild.innerHTML}
+              icon={post.querySelector('.ctaLink').getAttribute('title').split('/')[3]}
               />
-            {/* ))} */}
+            ))} 
           </div>
         </div>
       </div>
