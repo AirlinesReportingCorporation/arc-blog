d814ee223eb301ac1120d06c6e4c2f7893fa6bb4
Bree Armstrong <brianna.m.armstrong@gmail.com>
2022-11-10 15:47:03 -0500

    fix filter function and adjust show more

--- src/app.jsx
+++ src/app.jsx
@@ -9,26 +9,62 @@ class Blog extends Component {
       posts: [],
       curIndex: 6,
       prevIndex: 0,
-      filter: "data",
+      filter: "",
       filteredIndex: 0,
     };
   }
 
   componentDidMount() {
-    this.getPosts(this.state.prevIndex, this.state.curIndex);
+    if (this.state.filter == "") {
+      this.getPosts(this.state.prevIndex, this.state.curIndex);
+    } else {
+      this.getFilteredPosts(this.state.filteredIndex);
+    }
   }
 
   getPosts = (startIndex, endIndex) => {
     var postArray = document.querySelectorAll(
       ".content-block--pageItem__inside"
     );
-    //  Check to see if the filter is an empty string
-    if (this.state.filter == "") {
-      let i = startIndex;
-      console.log(i);
-      while (i < endIndex) {
-        const post = postArray[i];
-        var tempPosts = this.state.posts;
+    let i = startIndex;
+    console.log(i);
+    while (i < endIndex) {
+      const post = postArray[i];
+      var tempPosts = this.state.posts;
+      tempPosts.push({
+        link: post.querySelector(".ctaLink").getAttribute("href"),
+        title: post.querySelector(".ctaLink").getAttribute("title"),
+        tags: post
+          .querySelector(".content-block--pageItem__metadata")
+          .firstElementChild.innerHTML.split(","),
+        date: post.querySelector(".content-block--pageItem__metadata")
+          .lastElementChild.innerHTML,
+        icon: post.querySelector(".ctaLink").getAttribute("href").split("/")[3],
+      });
+      i++;
+    }
+    this.setState({ posts: tempPosts });
+    // We return the next 6 items -- the first time running will be 0 - 5th index
+    console.log("empty, return all");
+    // Notes:
+    // We may need to change the show more function to reflect if there is a filter as well otherwise its only showing potentially any within the next 6 indecies.
+  };
+
+  getFilteredPosts = (startIndex) => {
+    var postArray = document.querySelectorAll(
+      ".content-block--pageItem__inside"
+    );
+    let i = startIndex;
+    let tempIndex = 0;
+    while (i < postArray.length && tempIndex < 6) {
+      const post = postArray[i];
+      var tempPosts = this.state.posts;
+      if (
+        post
+          .querySelector(".content-block--pageItem__metadata")
+          .firstElementChild.innerText.toLowerCase()
+          .indexOf(this.state.filter) > -1
+      ) {
         tempPosts.push({
           link: post.querySelector(".ctaLink").getAttribute("href"),
           title: post.querySelector(".ctaLink").getAttribute("title"),
@@ -42,65 +78,38 @@ class Blog extends Component {
             .getAttribute("href")
             .split("/")[3],
         });
+        console.log(tempPosts);
+        tempIndex++;
         i++;
+        // maybe can use tempindex to create an alternative show more?
+        console.log("Temp Index" + tempIndex);
+        console.log("while index" + i);
+      } else {
+        i++;
+        console.log("Temp Index" + tempIndex);
+        console.log("while index" + i);
       }
-      this.setState({ posts: tempPosts });
-      // We return the next 6 items -- the first time running will be 0 - 5th index
-      console.log("empty, return all");
+      this.setState({ filteredIndex: i });
     }
-    // Check the post tags to see if it is inside the post
-    else {
-      let i = startIndex;
-      let tempIndex = 0;
-      while (i < postArray.length && tempIndex < 6) {
-        const post = postArray[i];
-        var tempPosts = this.state.posts;
-        if (
-          post
-            .querySelector(".content-block--pageItem__metadata")
-            .firstElementChild.innerText.toLowerCase()
-            .indexOf(this.state.filter) > -1
-        ) {
-          tempPosts.push({
-            link: post.querySelector(".ctaLink").getAttribute("href"),
-            title: post.querySelector(".ctaLink").getAttribute("title"),
-            tags: post
-              .querySelector(".content-block--pageItem__metadata")
-              .firstElementChild.innerHTML.split(","),
-            date: post.querySelector(".content-block--pageItem__metadata")
-              .lastElementChild.innerHTML,
-            icon: post
-              .querySelector(".ctaLink")
-              .getAttribute("href")
-              .split("/")[3],
-          });
-          console.log(tempPosts);
-          tempIndex++;
-          i++;
-          this.setState({filteredIndex: tempIndex})
-          console.log("Temp Index" + tempIndex);
-          console.log("while index" + i);
-        } else {
-          i++;
-          console.log("Temp Index" + tempIndex);
-          console.log("while index" + i);
-        }
-      }
-      this.setState({ posts: tempPosts });
-      // if it is then push that post to the array and increment the num of posts until we have 6
-      // otherwise keep going through the array until we get 6 more.
-      console.log("There is a filter: " + this.state.filter);
-    }
-
-    // Notes:
-    // We may need to change the show more function to reflect if there is a filter as well otherwise its only showing potentially any within the next 6 indecies.
+    this.setState({ posts: tempPosts });
+    // if it is then push that post to the array and increment the num of posts until we have 6
+    // otherwise keep going through the array until we get 6 more.
+    console.log("There is a filter: " + this.state.filter);
   };
 
   showMore = () => {
-    var tempIndex = this.state.curIndex;
-    this.setState({ prevIndex: tempIndex, curIndex: (tempIndex += 6) }, () => {
-      this.getPosts(this.state.prevIndex, this.state.curIndex);
-    });
+    if (this.state.filter == "") {
+      var tempIndex = this.state.curIndex;
+      this.setState({ prevIndex: tempIndex, curIndex: (tempIndex += 6) }, () => {
+        this.getPosts(this.state.prevIndex, this.state.curIndex);
+      });
+      console.log("show more with no filter")
+    } else {
+      console.log(this.state.filteredIndex + "from show more")
+      var tempIndex = this.state.filteredIndex
+      console.log(tempIndex + "from show more")
+      this.getFilteredPosts(tempIndex);
+    }
     console.log("click");
   };
 
