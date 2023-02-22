bb959be1bc2619edba052e1e5923a3aca5b26bd1
Bree Armstrong <brianna.m.armstrong@gmail.com>
2023-02-22 11:48:45 -0500

    add in hover effect for advert

--- src/components/AdvertPost.jsx
+++ src/components/AdvertPost.jsx
@@ -24,14 +24,14 @@ class AdvertPost extends Component {
               overflow: "hidden",
             }}
           >
-            <motion.div className="advertInner">
+            <motion.div className="advertInner" whileHover={{ scale: 1.1 }}>
               <div className="advertTitle">{this.props.title}</div>
               <div className="advertText">{this.props.pretext} <span className="advertHighlight">{this.props.highlight}</span>{this.props.posttext}</div>
-              <a href={this.props.link}>
+            </motion.div>
+            <a href={this.props.link}>
               <div className="advertCta"> Refund Information
               </div>
               </a>
-            </motion.div>
           </div>
         </a>
       </div>
